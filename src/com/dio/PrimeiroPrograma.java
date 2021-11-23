177a06a8eed753cf7c8bd69f55221019a14f3add
ArdyGoncalves <ardygoncalves1987@gmail.com>
2021-11-23 13:13:22 -0300

    First commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,18 @@
+package com.dio;
+
+import br.com.dio.model.*;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Gato gato = new Gato();
+		Livro livro = new livro();
+					
+		System.out.println(gato);
+		System.out.println(livro);
+
+	}
+
+}
